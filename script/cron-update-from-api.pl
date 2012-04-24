dbbb0d2c6bb78c5210c4c54f01bb9828f581a318
Rob Brown <rob@intelcompute.com>
2012-04-24 15:02:16 +0100

    cleanup

--- script/cron-update-from-api.pl
+++ script/cron-update-from-api.pl
@@ -1,25 +1,6 @@
 #!/usr/bin/perl -w
 
-#use strict;
-#use warnings;
-
-# use Data::Dumper;
-
-
 use Catalyst::ScriptRunner;
 Catalyst::ScriptRunner->run('CPANMonitor', 'Monitor');
 
-
-
-#use CPANMonitor::Schema;
-
-#my $schema = CPANMonitor::Schema->connect( 'dbi:mysql:cpanmonitor_dev', 'cpanmonitor_dev', 'cpanmonitor_dev' );
-
-#my @alerts = $schema->resultset('Alert')->all;
-
-#foreach my $alert ( @alerts )
-#{
-#	$alert->update_from_api;
-#}
-
-
+1;
