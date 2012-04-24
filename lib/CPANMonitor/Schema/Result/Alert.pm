6ca21af4a396d11d64fedc22484d344eb86b6fce
Rob Brown <rob@intelcompute.com>
2012-04-24 18:37:00 +0100

    Renaming cron job

--- lib/CPANMonitor/Schema/Result/Alert.pm
+++ lib/CPANMonitor/Schema/Result/Alert.pm
@@ -222,6 +222,7 @@ sub update_from_api
 			foreach my $user_alert ( $self->user_alerts )
 			{
 				my $kit = Email::MIME::Kit->new( { source => 'mkits/alert.mkit' } );
+				
  				my $email = $kit->assemble( { alert => $distribution } );
 
 				sendmail( $email, { to => [ $user_alert->email ] } );
